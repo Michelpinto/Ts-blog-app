4a35eb1a39f6afa5a581002928468efcebc1a9af
michelpinto <michabbe9@gmail.com>
2022-10-26 18:04:01 +0200

    server and dependencies set up

---
+++ backend/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+const cors = require('cors');
+
+const { errorHandler } = require('./middleware/errorMiddleware');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/api/goals', require('./routes/blogRoutes'));
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
