05bbdee6acf04dc60e11e2ce679761ed7f46248d
michelpinto <michabbe9@gmail.com>
2022-10-30 21:16:30 +0200

    creating components and pages

---
+++ frontend/src/pages/blogs/Blogs.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { BlogsDiv, SingleBlogDiv, BlogDiv } from './styles';
+
+const Blogs: React.FC = () => {
+  return <div />;
+};
+
+export default Blogs;
---
+++ frontend/src/pages/newBlog/NewBlog.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const NewBlog: React.FC = () => {
+  return <div />;
+};
+
+export default NewBlog;
