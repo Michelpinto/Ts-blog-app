12f790d6188c178d7df8c91dfd3787ad9d9fe4ac
michelpinto <michabbe9@gmail.com>
2022-10-31 14:08:36 +0200

    changing the styling

--- frontend/src/pages/blogs/styles.ts
+++ frontend/src/pages/blogs/styles.ts
@@ -14,32 +14,21 @@ export const BlogsDiv = styled.div`
 
 export const BlogDiv = styled.div`
   height: 18rem;
-  border: solid 1px #bebebe;
-  border-radius: 5px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  padding: 1.5rem;
+  border-top: solid 0.1px #bebebe;
+  padding: 1.5rem 0 1.5rem 0;
 
   display: flex;
   flex-direction: column;
   gap: 1.5rem;
   cursor: pointer;
 
-  & p {
-    font-size: 1.6rem;
-    color: #bebebe;
-  }
-`;
-
-export const SingleBlogDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 5rem;
-
   & h1 {
-    font-weight: 400;
+    font-weight: 600;
   }
 
   & p {
-    font-size: 1.6rem;
+    font-size: 1.3rem;
+    color: #bebebe;
+    font-weight: 300;
   }
 `;
