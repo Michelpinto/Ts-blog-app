4687169d16ad219e750616223bc42cde51cfb49d
michelpinto <michabbe9@gmail.com>
2022-11-03 23:11:48 +0200

    rich text editor setup

--- frontend/src/pages/newBlog/NewBlog.tsx
+++ frontend/src/pages/newBlog/NewBlog.tsx
@@ -1,20 +1,18 @@
-import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
+import React, { useState, useEffect } from 'react';
+import { useQuill } from 'react-quilljs';
 import 'react-quill/dist/quill.snow.css';
+import { useSelector } from 'react-redux';
 
 import { Div, Form } from './styles';
 
 const NewBlog: React.FC = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const blogs = useSelector((state: any) => state.blogs.blogs);
 
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
     console.log(e.target.value);
-  };
-
-  const handleContentChange = (value: string) => {
-    setContent(value);
   };
 
   const modules = {
@@ -45,10 +43,48 @@ const NewBlog: React.FC = () => {
     'code-block',
   ];
 
+  const placeholder = "Let's write something epic...";
+
+  const { quillRef, quill } = useQuill({ modules, formats, placeholder });
+
+  useEffect(() => {
+    if (quill) {
+      quill.on('text-change', () => {
+        setContent(quill.root.innerHTML);
+      });
+    }
+  }, [quill]);
+
+  const createNewBlog = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // const blog = {
+    //   title,
+    //   content,
+    // };
+    // console.log(blog);
+
+    const response = await fetch('http://localhost:5000/api/blogs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    setTitle('');
+    quill?.setText('');
+  };
+
   return (
     <Div>
       <h1>Share your amazing ideas</h1>
-      <Form>
+      <Form onSubmit={createNewBlog}>
         <input
           onChange={handleTitleChange}
           value={title}
@@ -56,14 +92,9 @@ const NewBlog: React.FC = () => {
           placeholder='Title'
           type='text'
         />
-        <ReactQuill
-          value={content}
-          onChange={handleContentChange}
-          placeholder='Write something amazing...'
-          // theme='snow'
-          modules={modules}
-          formats={formats}
-        />
+        <div>
+          <div ref={quillRef} />
+        </div>
         <button type='submit'>Post blog</button>
       </Form>
     </Div>
