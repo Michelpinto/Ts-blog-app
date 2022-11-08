e6f6063998057a4494ee13189805eea13a0a2b94
michelpinto <michabbe9@gmail.com>
2022-11-08 11:37:53 +0200

    error handler fix

--- backend/controllers/blogControllers.ts
+++ backend/controllers/blogControllers.ts
@@ -1,76 +1,59 @@
 import { Request, Response } from 'express';
+const asyncHandler = require('express-async-handler');
 const Blog = require('../model/blogModel');
 // getBlogs
-const getBlogs = async (req: Request, res: Response) => {
-  try {
-    const blogs = await Blog.find().sort({ createdAt: -1 });
+const getBlogs = asyncHandler(async (req: Request, res: Response) => {
+  const blogs = await Blog.find().sort({ createdAt: -1 });
 
-    res.status(200).json(blogs);
-  } catch (error) {
-    console.log(error);
-  }
-};
+  res.status(200).json(blogs);
+});
 // createBlogs
-const setBlog = async (req: Request, res: Response) => {
-  const errorMessage: any[] = [];
-  try {
-    if (!req?.body?.text) {
-      res.status(400);
-      throw new Error('Please enter a goal');
-    }
+const setBlog = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.body.title || !req.body.text) {
+    res.status(400);
+    throw new Error('Title is required');
+  }
 
-    const blog = await Blog.create({
+  const blog = await Blog.create({
+    title: req.body.title,
+    text: req.body.text,
+  });
+
+  res.status(200).json(blog);
+});
+// updateBlog
+const updateBlog = asyncHandler(async (req: Request, res: Response) => {
+  const blog = await Blog.findById(req.params.id);
+
+  if (!blog) {
+    res.status(404);
+    throw new Error('Blog not found');
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    req.params.id,
+    {
       title: req.body.title,
       text: req.body.text,
-    });
+    },
+    { new: true }
+  );
 
-    res.status(200).json(blog);
-  } catch (error) {
-    console.log(error);
-    errorMessage.push(error);
-  }
-};
-// updateBlog
-const updateBlog = async (req: Request, res: Response) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
-
-    if (!blog) {
-      res.status(404);
-      throw new Error('Blog not found');
-    }
-
-    const updatedBlog = await Blog.findByIdAndUpdate(
-      req.params.id,
-      {
-        title: req.body.title,
-        text: req.body.text,
-      },
-      { new: true }
-    );
-
-    res.status(200).json(updatedBlog);
-  } catch (error) {
-    console.log(error);
-  }
-};
+  res.status(200).json(updatedBlog);
+});
 // deleteBlog
-const deleteBlog = async (req: Request, res: Response) => {
-  try {
-    const blog = await Blog.findById(req.params.id);
+const deleteBlog = asyncHandler(async (req: Request, res: Response) => {
+  const blog = await Blog.findById(req.params.id);
 
-    if (!blog) {
-      res.status(404);
-      throw new Error('Blog not found');
-    }
-
-    await blog.deleteOne();
-
-    res.status(200).json(req.params.id);
-  } catch (error) {
-    console.log(error);
+  if (!blog) {
+    res.status(404);
+    throw new Error('Blog not found');
   }
-};
+
+  await blog.deleteOne();
+
+  res.status(200).json(req.params.id);
+});
 
 module.exports = {
   getBlogs,
