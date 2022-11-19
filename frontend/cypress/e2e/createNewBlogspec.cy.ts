445596d14dcac90b2e0b2507328b92bf3f4b47d9
michelpinto <michabbe9@gmail.com>
2022-11-19 22:55:29 +0200

    cypress setup and first e2e test

--- frontend/cypress/e2e/createNewBlogspec.cy.ts
+++ frontend/cypress/e2e/createNewBlogspec.cy.ts
@@ -2,6 +2,10 @@ describe('empty spec', () => {
   it('Creates a blog', () => {
     cy.visit('http://localhost:3000/newBlog');
 
-    cy.get('#title').type('My first blog using cypress');
+    cy.get('[name="title"]').type('My first blog using cypress');
+    cy.get('.ql-editor').type('This is my first blog using cypress');
+    cy.get('[type="submit"]').click();
+    cy.hasVisibleText('Blogs').click();
+    cy.hasVisibleText('My first blog using cypress');
   });
 });
