8b5addae8f4c0b190b387315c43311ba04c8ba44
michelpinto <michabbe9@gmail.com>
2022-11-29 21:55:04 +0200

    adding a protect route function

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -5,10 +5,11 @@ const {
   getMe,
   loginUser,
 } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
 // register user
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 module.exports = router;
