aedcf2005729779490b6dd5e09cf24140b00f2f2
michelpinto <michabbe9@gmail.com>
2022-12-05 13:29:35 +0200

    feature(register): building the page

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { RegisterContainer, Form } from './registerStyles';
 
 const Register: React.FC = () => {
-  return <div />;
+  return (
+    <RegisterContainer>
+      <h1>Create an account</h1>
+      <Form>
+        <label htmlFor='name'>Name</label>
+        <input type='name' name='name' id='name' />
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' id='email' />
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' id='password' />
+        <button type='submit'>Create</button>
+      </Form>
+    </RegisterContainer>
+  );
 };
 
 export default Register;
---
+++ frontend/src/pages/register/registerStyles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const RegisterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  max-width: 50rem;
+
+  & h1 {
+    margin: 2rem 0;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  & label {
+    font-size: 1.6rem;
+  }
+
+  & input {
+    padding: 1rem;
+    font-size: 1.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.5rem;
+  }
+
+  & button {
+    padding: 1rem;
+    font-size: 1.5rem;
+    border: none;
+    border-radius: 0.5rem;
+    background-color: #1d1d1d;
+    color: #fff;
+    cursor: pointer;
+  }
+`;
