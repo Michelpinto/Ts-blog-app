73c496b3c33b63ea1c825dc6275bcfdaca259861
michelpinto <michabbe9@gmail.com>
2022-12-13 16:18:44 +0200

    adding values to inputs register page

--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { RegisterContainer, Form } from './registerStyles';
 
 const Register: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setFormData((prevState) => ({ ...prevState, [e.target.name]: value }));
+  };
+
   return (
     <RegisterContainer>
       <h1>Create an account</h1>
       <Form>
-        <label htmlFor='name'>Name</label>
-        <input type='name' name='name' id='name' />
-        <label htmlFor='email'>Email</label>
-        <input type='email' name='email' id='email' />
-        <label htmlFor='password'>Password</label>
-        <input type='password' name='password' id='password' />
+        <input
+          placeholder='Enter your name'
+          onChange={handleChange}
+          value={name}
+          type='text'
+          name='name'
+          id='name'
+        />
+        <input
+          placeholder='Enter your email'
+          onChange={handleChange}
+          value={email}
+          type='email'
+          name='email'
+          id='email'
+        />
+        <input
+          placeholder='Enter your password'
+          onChange={handleChange}
+          value={password}
+          type='password'
+          name='password'
+          id='password'
+        />
+        <input
+          placeholder='Confirm password'
+          onChange={handleChange}
+          value={password2}
+          type='password'
+          name='password2'
+          id='password2'
+        />
         <button type='submit'>Create</button>
       </Form>
       <p>
