8b32357091b03995b50242f6cd5ff45735b7dff5
michelpinto <michabbe9@gmail.com>
2022-12-26 18:31:17 +0200

    adding logout button to the nav

--- frontend/src/components/nav/Nav.tsx
+++ frontend/src/components/nav/Nav.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { InsideDiv, Div } from './styles';
+import { IoLogOutOutline } from 'react-icons/io5';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../../app/data/features/authSlice';
+import { AppDispatch } from '../../app/store';
 
 const Nav: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const { user } = useSelector((state: any) => state.auth);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/login');
+  };
+
   return (
     <Div>
       <Link to='/'>
@@ -16,6 +30,11 @@ const Nav: React.FC = () => {
         <Link to='/newBlog'>
           <h1>Create</h1>
         </Link>
+
+        <span onClick={handleLogout}>
+          Logout
+          <IoLogOutOutline />
+        </span>
       </InsideDiv>
     </Div>
   );
